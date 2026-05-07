88f7c59cde9283a7a887e38ed07cc3d2e5d78ce0
guto_stack <augustoccfaria@gmail.com>
2026-05-07 10:18:32 -0300

    alguns testes e alterações no frontend

---
+++ FrontEnd/src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home(){
+    return (
+          <nav className='flex gap-4 p-4'>
+                  <Link to='/'>Home</Link>
+                  <Link to='/login'>Login</Link>
+                </nav>
+    );
+}
+
+export default Home
---
+++ FrontEnd/src/Pages/Login.jsx
@@ -0,0 +1,15 @@
+function Login() {
+  return (
+    <div className="m-0 h-screen grid grid-cols-2">
+        <div className="bg-primary p-8 flex flex-col items-center justify-center">
+            <h1 className="items-center justify-center text-6xl font-bold text-text text-left">Arvore Dinossáurica</h1>
+            <h2 className="text-2xl text-lightprimary text-left">Árvore genealógica de dinossauros</h2>
+        </div>
+        <div className="bg-primary p-100">Collum2</div>
+
+    </div>
+  );
+
+}
+
+export default Login
