5cf164583f82d248f17ba276c6f4005d01d67f75
guto_stack <augustoccfaria@gmail.com>
2026-05-12 14:40:50 -0300

    alterações no visual da pagina de login

--- FrontEnd/src/Pages/Home.jsx
+++ FrontEnd/src/Pages/Home.jsx
@@ -1,9 +1,9 @@
 function Home(){
     return (
           <nav className='flex gap-4 p-4'>
-                  <Link to='/'>Home</Link>
-                  <Link to='/login'>Login</Link>
-                </nav>
+            <a href="">Home</a>
+            <a href="/Login">Login</a>
+          </nav>
     );
 }
 
