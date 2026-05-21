a09fe4688ec8e1e67d65076cf9bcd8be62ba2966
guto_stack <augustoccfaria@gmail.com>
2026-05-21 13:50:20 -0300

    adcionando logica no card de login

--- FrontEnd/src/Components/Botao.jsx
+++ FrontEnd/src/Components/Botao.jsx
@@ -1,6 +1,6 @@
-export function Botao({ children, onClick, className }) {
+export function Botao({ children, onClick, className, type = "button" }) {
     return (
-        <button onClick={onClick} className={className}>
+        <button type={type} onClick={onClick} className={className}>
             {children}
         </button>
     );
--- FrontEnd/src/Components/CardLogin.jsx
+++ FrontEnd/src/Components/CardLogin.jsx
@@ -1,26 +1,54 @@
 import { Input } from "./Input";
 import { Botao } from "./Botao";
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 
 
 export function CardLogin() {
 
+
   const navigate = useNavigate();
+  
+  const [usuario, setUsuario] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleSubmit =(e) => {
+    e.preventDefault();
+    if (usuario === "admin" && senha === "admin") {
+      navigate('/home');
+    } else {
+      alert("Usuário ou senha incorretos!");
+    }
+  };
+
     return (
+      <form action="" onSubmit={handleSubmit}>
         <div className="bg-background px-16 py-10 w-full h-full flex flex-col items-center justify-center gap-8">
           <span className="text-4xl font-bold text-text">Bem-vindo!</span>
           <div className="flex flex-col gap-4 bg-lightprimary p-8 rounded-lg shadow-md w-80 h-90">
             <nav className="text-2xl font-bold text-text text-center">Login</nav>
-            <Input type="text" placeholder="Usuário" className="w-full" />
-            <Input type="password" placeholder="Senha" className="w-full" />
-            <Botao className="bg-primary text-text font-bold py-2 px-4 rounded hover:bg-blue-700">Entrar</Botao>
+            <Input 
+              type="text" 
+              placeholder="Usuário" 
+              className="w-full" 
+              value={usuario}
+              onChange={(e) => setUsuario(e.target.value)}
+            />
+            <Input 
+              type="password" 
+              placeholder="Senha" 
+              className="w-full" 
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+            />
+            <Botao type="submit" className="bg-primary text-text font-bold py-2 px-4 rounded hover:bg-blue-700">Entrar</Botao>
             <div className="relative flex py-5 items-center">
               <div className="flex-grow border-t border-secondary"></div>
               <span className="flex-shrink mx-4 text-secondary">ou</span>
               <div className="flex-grow border-t border-secondary"></div>
             </div>
-            <Botao className="bg-secondary text-text font-bold py-2 px-4 rounded hover:bg-secondary/80"  onClick={() => navigate('/home')}>Entrar como Visitante</Botao>
+            <Botao type="button" className="bg-secondary text-text font-bold py-2 px-4 rounded hover:bg-secondary/80" onClick={() => navigate('/home')}>Entrar como Visitante</Botao>
           <div className="flex gap-3 mt-4 relative whitespace-nowrap  items-center justify-center ">
             <Link to="/esqueci_minhasenha" className="text-text hover:underline mr-4">
               Esqueci minha senha
@@ -31,5 +59,6 @@ export function CardLogin() {
           </div>
           </div>
         </div>
+        </form>
     );
 }
